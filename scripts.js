c1083904000732dc0dd5498b77039d0dc68ee7f9
Fanghan Hu <Fanghan.Hu@gmail.com>
2020-11-16 19:13:59 -0600

    Display query results

--- scripts.js
+++ scripts.js
@@ -3,16 +3,23 @@ let $searchBtn = $('#searchBtn');
 let $history = $('#history');
 let $forecast = $('#forecast');
 
+//current data display:
+let $cityName = $('#cityName');
+let $temperature = $('#temperature');
+let $humidity = $('#humidity');
+let $windSpeed = $('#windSpeed');
+let $uvIndex = $('#uvIndex');
+
 //Open Weather API Key
 const apiKey = "12a86102d69b851ad68961e1b9b10e46";
 
 
-function createForecastCard(date, weather, temperature, humidity) {
+function createForecastCard(date, weatherIconCode, temperature, humidity) {
     return $(`
         <div class="card m-2 p-1 bg-primary text-white forecast-card">
             <div class="card-title">${date}</div>
-            <div class="card-body">
-                <i class="fa fa-cloud"></i>
+            <div class="card-body text-center">
+                <img src="http://openweathermap.org/img/wn/${weatherIconCode}.png" alt="weather">
                 <p>Temp: ${temperature} °F</p>
                 <p>Humidity: ${humidity}%</p>
             </div>
@@ -42,16 +49,19 @@ function searchForCity(searchTerm) {
         method: "GET"
     }).done((response) => {
         // noinspection JSUnresolvedVariable
-        getForecast(response.coord);
+        getForecast(response.coord, response.name);
     }).catch((e) => {
         if(e.status === 404) {
             //TODO: Can not find city
-
+            alert("Cannot find the given city!");
         }
     });
 }
 
-function getForecast(coordinates) {
+/**
+ * request the full forecast data with the city coordinates
+ */
+function getForecast(coordinates, cityName) {
     // noinspection JSUnresolvedVariable
     let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`
 
@@ -60,9 +70,34 @@ function getForecast(coordinates) {
         method: "GET"
     }).done((response) => {
         console.log(response);
+        //display current data
+        $cityName.text(cityName + "(" + getDate(response.current.dt) + ")");
+        $temperature.text(kelvinToFahrenheit(response.current.temp));
+        $humidity.text(response.current.humidity);
+        $windSpeed.text(response.current.wind_speed);
+        $uvIndex.text(response.current.uvi);
+
+        //display forecast data:
+        $forecast.empty();
+        for (let i = 1; i < response.daily.length && i < 6; i++) {
+            let dailyData = response.daily[i];
+            $forecast.append(createForecastCard(
+                getDate(dailyData.dt),
+                dailyData.weather[0].icon,
+                kelvinToFahrenheit(dailyData.temp.day),
+                dailyData.humidity));
+        }
     });
 }
 
+//convert unix time to a readable date
+function getDate(dt) {
+    let date = new Date(dt * 1000);
+    return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
+}
+
+
+//handles the searches
 $search.change(handleSearch);
 $searchBtn.click(handleSearch);
 
